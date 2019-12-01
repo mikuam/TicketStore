8504b598bb943fe8528a8e944ce477398d2fbe6e
Michał Białecki <michal.bialecki@guestline.com>
2019-12-01 20:36:46 +0100

    Added TicketsController and validation

---
+++ TicketStore/TicketStore/Controllers/TicketsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace TicketStore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        [Route("BuyTicket")]
+        [HttpPost]
+        public IActionResult BuyTicket(Ticket ticket)
+        {
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
---
+++ TicketStore/TicketStore/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketStore
+{
+    public class Ticket
+    {
+        [MinLength(1)]
+        public string MovieTitle { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Row { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Seat { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+    }
+}
